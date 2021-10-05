b115009f55628d4fc125343265041f1aa62be742
loonatiq <86408147+loonatiq@users.noreply.github.com>
2021-10-05 23:11:16 +0900

    2nd commit

--- toDoList.js
+++ toDoList.js
@@ -1,12 +1,22 @@
+//Justifing Const
 const toDoList = document.querySelector('#tdl');
-const tBox = document.getElementById('newThing');
-document.getElementById('addBtn').onclick = addNew;
+const tBox = document.querySelector('#newThing');
 
+//Event Handler
+document.querySelector('#addBtn').addEventListener('click', addNew);
+document.querySelector('#addBtn').addEventListener('keydown', function(){
+  if(keyCode === '13'){
+  }
+});
+
+//Functions
+//내용 있을 시 리스트 추가 가능
 function addNew(){
+  if(tBox.value !== ''){
   let inputValue = tBox.value;
   const thing = document.createElement('li');
   thing.textContent = inputValue;
   toDoList.append(thing);
-  tBox.value = '';
+  tBox.value = '';}
 }
 
