71e42308bc750f77408d2a1a5a0ed90a60d03d41
loonatiq <86408147+loonatiq@users.noreply.github.com>
2021-10-07 18:23:09 +0900

    4th commit

--- toDoList.js
+++ toDoList.js
@@ -16,21 +16,37 @@ document.querySelector('#newThing').addEventListener('keypress', function(e){
 // 1. 내용 있을 시 리스트 추가 가능
 function addNew(){
   if(tBox.value !== ''){
-  let inputValue = tBox.value;
-  const thing = document.createElement('li');
-  thing.textContent = inputValue;
-  toDoList.append(thing);
-  tBox.value = '';
-// 1-1. 리스트 완료 표시
-for(let i = 0; i < things.length; i++){
-  things[i].addEventListener('click', done);
-}
-}
+    // 1-1. 리스트 컨테이너
+    const item = document.createElement('div');
+    toDoList.append(item);
+    item.classList.add('item');
+    // 1-2. 리스트 내용
+    let inputValue = tBox.value;
+    const thing = document.createElement('li');
+    thing.textContent = inputValue;
+    item.append(thing);
+    tBox.value = '';
+    // 1-3. 삭제 아이콘
+    const icon = document.createElement('i');
+    icon.setAttribute('class', 'fas fa-minus-square');
+    item.append(icon);
+    // 1-4. 리스트 완료 표시 요소 선택
+      for(let i = 0; i < things.length; i++){
+        things[i].children[0].addEventListener('click', done);
+      }
+    // 1-5. 리스트 삭제 요소 선택
+      for(let i = 0; i < things.length; i++){
+        things[i].children[1].addEventListener('click', deleteList);
+      }
+  }
 }
 
 // 2. 리스트 완료 표시
-function done(event){
+function done(){
   this.classList.toggle('done');
 }
 
-
+// 3. 리스트 삭제
+function deleteList(){
+  this.parentElement.remove();
+}
